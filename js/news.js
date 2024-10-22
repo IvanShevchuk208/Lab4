dd29642cdbe8df3d6fa8c5801f930eb3cc46f896
IvanShevchuk208 <shevchuk.ivan.o@chnu.edu.ua>
2024-10-22 18:17:27 +0300

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,33 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('scroll', function() {
+    const scrollButton = document.querySelector('.scroll-to-top');
+    if (window.scrollY > 300) {
+        scrollButton.classList.add('show');
+    } else {
+        scrollButton.classList.remove('show');
+    }
+});
+
+var modal = document.getElementById("authModal");
+var btn = document.getElementById("signInBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
